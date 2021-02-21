93c6288d5901a313b4aa4575589c07b5987f10fb
Ralitsa Todorova <r.v.todorova94@gmail.com>
2021-02-21 22:05:06 +0200

    done to the percentage

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Calculator/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Calculator}
 }
 </script>
 
---
+++ src/components/Calculator.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="calculator">
+    <div class="display">{{current || '0'}}</div>
+    <div class='btn' @click="clear">C</div>
+    <div class='btn' @click="sign">+/-</div>
+    <div class='btn' @click="percent">%</div>
+    <div class='btn operator' @click="divide">÷</div>
+    <div class='btn'>7</div>
+    <div class='btn'>8</div>
+    <div class='btn'>9</div>
+    <div class='btn operator'>x</div>
+    <div class='btn'>4</div>
+    <div class='btn'>5</div>
+    <div class='btn'>6</div>
+    <div class='btn operator'>-</div>
+    <div class='btn'>1</div>
+    <div class='btn'>2</div>
+    <div class='btn'>3</div>
+    <div class='btn operator'>+</div>
+    <div class="zero btn">0</div>
+    <div class='btn'>.</div>
+    <div class='btn operator'>=</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  data(){
+    return{
+      current:'123'
+    }
+  },
+  methods:{
+    clear(){
+      this.current=''
+    },
+    sign(){
+      this.current = this.current.charAt(0)==='-' ? this.current.slice(1) : `-${this.current}`
+    },
+    percent(){
+      this.current=`${parseFloat(this.current)/100}`
+    }
+  }
+  
+}
+</script>
+
+
+<style scoped>
+.calculator{
+  display:grid;
+  grid-template-columns: repeat(4,1fr);
+  grid-auto-rows: minmax(50px, auto);
+  font-size:40px;
+  width:350px;
+  margin:0 auto;
+  grid-gap:5px;
+ 
+}
+.display{
+  grid-column:1/5;
+  border:solid #999 1px;
+   border-radius:10px;
+   margin-bottom:2px;
+}
+.zero{
+  grid-column:1/3;
+}
+.btn{
+  background-color:#eee;
+  border:1px solid #999;
+  border-radius:10px;
+  cursor:pointer;
+}
+.operator{
+  background-color: orange;
+  color:#fff;
+}
+</style>
